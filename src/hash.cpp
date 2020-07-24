0ec3d428a7ffa46fd5535b71d42ef87aabdc7f69
Not Available <masterofnets2@protonmail.com>
2020-07-24 08:54:55 +0000

    Update hash.cpp

--- src/hash.cpp
+++ src/hash.cpp
@@ -41,11 +41,11 @@ unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char
     switch (vDataToHash.size() & 3) {
         case 3:
             k1 ^= tail[2] << 16;
-        break;
+        /* FALLTHROUGH */
 
         case 2:
             k1 ^= tail[1] << 8;
-        break;
+        /* FALLTHROUGH */
 
         case 1:
             k1 ^= tail[0];
@@ -53,7 +53,7 @@ unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char
             k1 = ROTL32(k1, 15);
             k1 *= c2;
             h1 ^= k1;
-        break; 
+        /* FALLTHROUGH */ 
     }
 
     //----------
