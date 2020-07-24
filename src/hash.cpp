6e314ecb2acb6c0651264d3221be12f684589f4f
Not Available <masterofnets2@protonmail.com>
2020-07-24 09:07:25 +0000

    Update hash.cpp

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,34 +1,34 @@
 #include "hash.h"
 
 
-inline uint32_t ROTL32 ( uint32_t x, int8_t r )
+inline uint64_t ROTL64 ( uint64_t x, int8_t r )
 {
-    return (x << r) | (x >> (32 - r));
+    return (x << r) | (x >> (64 - r));
 }
 
 unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char>& vDataToHash)
 {
-    // The following is MurmurHash3 (x86_32), see http://code.google.com/p/smhasher/source/browse/trunk/MurmurHash3.cpp
-    uint32_t h1 = nHashSeed;
-    const uint32_t c1 = 0xcc9e2d51;
-    const uint32_t c2 = 0x1b873593;
+    // The following is MurmurHash3 (x86_64), see http://code.google.com/p/smhasher/source/browse/trunk/MurmurHash3.cpp
+    uint64_t h1 = nHashSeed;
+    const uint64_t c1 = 0xcc9e2d51;
+    const uint64_t c2 = 0x1b873593;
 
     const int nblocks = vDataToHash.size() / 4;
 
     //----------
     // body
-    const uint32_t * blocks = (const uint32_t *)(&vDataToHash[0] + nblocks*4);
+    const uint64_t * blocks = (const uint64_t *)(&vDataToHash[0] + nblocks*4);
 
     for(int i = -nblocks; i; i++)
     {
-        uint32_t k1 = blocks[i];
+        uint64_t k1 = blocks[i];
 
         k1 *= c1;
-        k1 = ROTL32(k1,15);
+        k1 = ROTL64(k1,15);
         k1 *= c2;
 
         h1 ^= k1;
-        h1 = ROTL32(h1,13); 
+        h1 = ROTL64(h1,13); 
         h1 = h1*5+0xe6546b64;
     }
 
@@ -36,7 +36,7 @@ unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char
     // tail
     const uint8_t * tail = (const uint8_t*)(&vDataToHash[0] + nblocks*4);
 
-    uint32_t k1 = 0;
+    uint64_t k1 = 0;
 
     switch (vDataToHash.size() & 3) {
         case 3:
@@ -50,7 +50,7 @@ unsigned int MurmurHash3(unsigned int nHashSeed, const std::vector<unsigned char
         case 1:
             k1 ^= tail[0];
             k1 *= c1;
-            k1 = ROTL32(k1, 15);
+            k1 = ROTL64(k1, 15);
             k1 *= c2;
             h1 ^= k1;
         /* FALLTHROUGH */ 
